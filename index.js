6a8c4608bdb8132a06de07b4815015d735e5c6ca
Hshuttle <huntershutle@gmail.com>
2022-10-23 13:13:12 -0400

    broke out questions into 3 groups

--- index.js
+++ index.js
@@ -19,23 +19,95 @@ const managerquestions = [
   },
   {
     type: "input",
-    message: "What is your team manager's name?",
-    name: "managername",
-  },
-  {
-    type: "input",
     message: "What is your team manager's office phone number?",
     name: "managerphone",
   },
+  {
+    type: "list",
+    message: "Would you like to add any additional team members?",
+    choices: ["Engineer", "Intern", "No one else to add"],
+    name: "manageradd",
+  },
 ];
+
+const engineerquestions = [
+  {
+    type: "input",
+    message: "What is your engineer's name?",
+    name: "engineername",
+  },
+  {
+    type: "input",
+    message: "What is your engineer's id number?",
+    name: "engineerid",
+  },
+  {
+    type: "input",
+    message: "What is your engineer's email?",
+    name: "engineeremail",
+  },
+  {
+    type: "input",
+    message: "What is your engineer's GitHub username?",
+    name: "engineergithub",
+  },
+  {
+    type: "list",
+    message: "Would you like to add any additional team members?",
+    choices: ["Engineer", "Intern", "No one else to add"],
+    name: "engineeradd",
+  },
+];
+
+const internquestions = [
+  {
+    type: "input",
+    message: "What is your intern's name?",
+    name: "internname",
+  },
+  {
+    type: "input",
+    message: "What is your intern's id number?",
+    name: "internid",
+  },
+  {
+    type: "input",
+    message: "What is your intern's email?",
+    name: "internemail",
+  },
+  {
+    type: "input",
+    message: "What is your interns school?",
+    name: "internschool",
+  },
+  {
+    type: "list",
+    message: "Would you like to add any additional team members?",
+    choices: ["Engineer", "Intern", "No one else to add"],
+    name: "internadd",
+  },
+];
+
+console.log("");
 console.log("Please follow the prompts below to build your team!");
+console.log("");
+
 // inquirer.prompt(managerquestions).then((answers) => {
-//   fs.writeFileSync(path.join(__dirname, "/dist/", "teammembers.html"));
+//   const FILENAME =
+//     answers.managername.toLowerCase().split(" ").join("") + ".json";
+//   fs.writeFile(FILENAME, JSON.stringify(answers), (err) => {
+//     err ? console.error(err) : console.log("Successfully wrote to file");
+//   });
 // });
-inquirer.prompt(managerquestions).then((answers) => {
-  const FILENAME =
-    answers.managername.toLowerCase().split(" ").join("") + ".json";
-  fs.writeFile(FILENAME, JSON.stringify(answers), (err) => {
-    err ? console.error(err) : console.log("Successfully wrote to file");
+function init() {
+  inquirer.prompt(managerquestions).then((answers) => {
+    if ((answers.memberType = "Engineer")) {
+      inquirer.prompt(engineerquestions).then((answers) => {});
+    } else if ((answers.memberType = "Intern")) {
+      inquirer.prompt(internquestions).then((answers) => {});
+    } else {
+    }
   });
-});
+}
+
+init();
